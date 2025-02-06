7e73654a7c775f0b89da73d16035b863b4b113a3
locedu <locutus.edu@outlook.com>
2025-02-05 23:28:11 -0500

    working comment routes..

--- src/routes/comments/commentRoutes.js
+++ src/routes/comments/commentRoutes.js
@@ -7,5 +7,8 @@ const { protect, checkOwnership } = require('../../middleware/authMiddleware');
 // Get my comments..
 router.get('/me', protect, commentController.getMyComments);
 
+// PUT edit my comments..
+router.put("/:commentId", protect, checkOwnership(), commentController.updateComment);
+
 
 module.exports = router;
